4faba86ea77b338c2276f2a202630ea7f4ef4405
jizefeng <205402866@qq.com>
2025-05-27 22:51:58 +0800

    点位管理改造-区域详情

--- src/views/manger/region/index.vue
+++ src/views/manger/region/index.vue
@@ -65,8 +65,9 @@
       <el-table-column label="备注说明" align="center" prop="remark" />
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template #default="scope">
-          <el-button link type="primary"  @click="handleUpdate(scope.row)" v-hasPermi="['manage:region:edit']">修改</el-button>
-          <el-button link type="primary"  @click="handleDelete(scope.row)" v-hasPermi="['manage:region:remove']">删除</el-button>
+          <el-button link type="primary"  @click="getRegionInfo(scope.row)" v-hasPermi="['manger:region:list']">查看详情</el-button>
+          <el-button link type="primary"  @click="handleUpdate(scope.row)" v-hasPermi="['manger:region:edit']">修改</el-button>
+          <el-button link type="primary"  @click="handleDelete(scope.row)" v-hasPermi="['manger:region:remove']">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -96,11 +97,25 @@
         </div>
       </template>
     </el-dialog>
+<!--    查看详情对话框-->
+    <el-dialog title="查看详情" v-model="regionInfoOpen" width="500px" append-to-body>
+      <el-form-item label="区域名称" prop="regionName">
+        <el-input v-model="form.regionName" disabled />
+      </el-form-item>
+      <label>包含点位：</label>
+      <el-table :data="nodeList">
+        <el-table-column label="序号" type="index" width="50" align="center" prop="id"/>
+        <el-table-column label="点位名称" align="center" prop="nodeName" />
+        <el-table-column label="设备数量" align="center" prop="vmCount" />
+      </el-table>
+    </el-dialog>
   </div>
 </template>
 
 <script setup name="Region">
 import { listRegion, getRegion, delRegion, addRegion, updateRegion } from "@/api/manger/region";
+import {listNode} from "@/api/manger/node.js";
+import {loadAllParams} from "@/api/page.js";
 
 const { proxy } = getCurrentInstance();
 
@@ -238,6 +253,23 @@ function handleExport() {
     ...queryParams.value
   }, `region_${new Date().getTime()}.xlsx`)
 }
+/** 查看详情按钮操作 */
+const nodeList = ref([]);
+const regionInfoOpen = ref(false);
+function getRegionInfo(row) {
+  //  查询区域详情
+  reset();
+  const _id = row.id
+  getRegion(_id).then(response => {
+    form.value = response.data;
+  });
+  // 查询点位列表
+  loadAllParams.regionId = row.id;
+  listNode(loadAllParams).then(response => {
+    nodeList.value = response.rows;
+  })
+  regionInfoOpen.value = true;
 
+}
 getList();
 </script>
